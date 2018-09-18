bae6af995f1cb0098611e23692be571577474aed
William Carter <wrcarter@cougar.cs.ksu.edu>
2018-09-18 15:37:19 -0500

    commit 2

--- core.c
+++ core.c
@@ -30,36 +30,55 @@ int is_number_convertable(const char * string) {
         }
         s++;
     }
-    
     return TRUE;
 }
 
 char encrypt_character(char source, int key) {
    if(source){
 	char encrypted;
-	encrypted = source+key;
-	return encrypted;
+	if(source+key >= 'Z'){
+		encrypted = source+key-CHARACTER_COUNT;
+		return encrypted;
+	}
+	else{
+		encrypted = source+key;
+		return encrypted;
+	}
    }
 	return FALSE
 }
 
 void encrypt_string(char * message, int key, int step) {
 	if(message){
-		char encrypted[strlen(message)];
-		for(int i = 0; message[0] != '\0'; i++){
-		encrypted[i] = encrypt_character(message[i], key+(step*i));
+		char encrypted[strlen(*message)];
+		for(int i = 0; message[i] != '\0'; i++){
+			encrypted[i] = encrypt_character(message[i], key+(step*i));
 		}
-	printf(encrypted);
+	printf("%s", encrypted);
 	}
-	
 
 }
 
 char decrypt_character(char source, int key) {
-    return source;
+	if((source-key)< 'A'){
+		source = CHARACTER_COUNT-(abs(source-key));
+	}
+	else{
+		source = source-key;
+	}
+	 return source;
 }
 
 void decrypt_string(char * message, int key, int step) {
+	int count=0;
+	for(count; message[count] != '/0'; count++);
+	char decrypted[count];
+	int length = count;
+	for(count; count>=0; count--){
+		decrypted[length-count]= decrypt_character(message[count], (step+key*count));
+	}
+	printf("%s", decrypted);
+
 }
 
 void print_usage() {
@@ -69,6 +88,16 @@ void print_usage() {
 }
 
 int core_main(int argc, const char * argv[]) {
+	if(argv[1] = "encrypt"){
+		encrypt_string(* argv[2],(int) *argv[3], (int) *argv[4]);
+	}
+	else if(argv[1] = "decrypt"){
+		decrypt_string(* argv[2],(int) *argv[3],(int) *argv[4]);
+	}
+	else{
+		printf("%s %d", "Unknown command error: ", *argv[1]);
+	}
+
     print_usage();
     return 0;
 }
